33376a9d09fa04d1419fc5e869c42d48367a285e
Rikikudo <trunghieu99tt@gmail.com>
2022-11-15 20:53:55 +0700

    debug: cloudfront - 1

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
     return (
         <div>
-            <h1>Hi there from App!!!!</h1>
+            <h1>Hi there from Container !!!!</h1>
             <hr />
             <MarketingApp />
         </div>
--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
                             color="textPrimary"
                             gutterBottom
                         >
-                            Home Page From Marketing
+                            Home Page From Marketing Page
                         </Typography>
                         <Typography
                             variant="h5"
