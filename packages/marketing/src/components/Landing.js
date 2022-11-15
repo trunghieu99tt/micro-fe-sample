05e83470ee72fea836041bdc6709e430650301e0
Rikikudo <trunghieu99tt@gmail.com>
2022-11-15 20:57:27 +0700

    debug: cloudfront - 2

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
                             color="textPrimary"
                             gutterBottom
                         >
-                            Home Page From Marketing Page
+                            Home Page From Marketing Page 1
                         </Typography>
                         <Typography
                             variant="h5"
