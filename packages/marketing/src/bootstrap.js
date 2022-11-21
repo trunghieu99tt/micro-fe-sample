32b3fb83a488d78397d21126bdbe3bb6a723cad6
Rikikudo <trunghieu99tt@gmail.com>
2022-11-22 04:13:59 +0700

    fix: routing issue

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const el = document.getElementById("_marketing-dev-root");
   if (el) {
-    mount(el);
+    mount(el, { defaultHistory: createBrowserHistory() });
   }
 }
 
